93c3f083d9322fd49f6e6601fa70bc9e7693ed37
loganwong52 <loganwong52@gmail.com>
2024-01-10 16:59:35 -0800

    ball moves right when space pressed

--- ball.py
+++ ball.py
@@ -3,6 +3,7 @@ import pygame
 w = 640
 h = 480
 halfway = h / 2
+amount = 10
 
 
 class Ball(pygame.sprite.Sprite):
@@ -11,7 +12,30 @@ class Ball(pygame.sprite.Sprite):
 
         self.image = pygame.Surface((20, 20))
         self.image.fill("Yellow")
-        self.rect = self.image.get_rect(center=(w / 2, halfway))
+        self.rect = self.image.get_rect(midleft=(20, halfway))
+
+        self.direction = ""
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            # space key is pressed, launch ball
+            self.direction = "right"
+
+    def move_right(self):
+        # Moves to the right until it hits the opponent
+        # while self.rect.x < w:
+        self.rect.x += amount
+
+        if self.rect.x >= w:
+            print("game over")
+            self.direction = ""
 
     def update(self):
-        pass
+        self.player_input()
+
+        if self.direction == "right":
+            self.move_right()
+        elif self.direction == "left":
+            # self.move_left()
+            pass
